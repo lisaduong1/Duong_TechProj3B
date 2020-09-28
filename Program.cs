9e5c8f5434ae1945891b8c0567707d1eee8d5234
Lisa Duong <lisaduong@usf.edu>
2020-09-27 21:56:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/*
+ * Name: Lisa Duong
+ * Assignment: Tech Project #3B
+ * Date: 9/27/20
+ * Class: ISM4300.001
+ * Description: Create a C# console application that demonstrates an Array
+ */
+namespace Duong_TechProj3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+            //manually populated array
+            foreach (int i in numbers)
+            {
+                Console.WriteLine("Element Value = " + i);
+            }
+                Console.WriteLine("Press any key to exit program...");
+            //foreach loop to insert new number for every line
+        }
+    }
+}
